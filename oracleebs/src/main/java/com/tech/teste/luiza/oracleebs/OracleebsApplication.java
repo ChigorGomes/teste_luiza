87efaf507736787bdea333aad50197c43d90cce7
Cicero <chgs@icomp.ufam.edu.br>
2025-01-24 21:31:44 -0400

    feat: configuração inicial do projeto - Adicionado docker-compose.yml com serviços PostgreSQL e RabbitMQ. - Configurado application.yml com propriedades para PostgreSQL e RabbitMQ. - Adicionado as bibliotecas principais do projeto.

---
+++ oracleebs/src/main/java/com/tech/teste/luiza/oracleebs/OracleebsApplication.java
@@ -0,0 +1,13 @@
+package com.tech.teste.luiza.oracleebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleebsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleebsApplication.class, args);
+	}
+
+}
---
+++ oracleebs/src/test/java/com/tech/teste/luiza/oracleebs/OracleebsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.teste.luiza.oracleebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleebsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
