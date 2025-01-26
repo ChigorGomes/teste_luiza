2765c5aa116d4acf7274934f3b6bef51f0934a34
Cicero <chgs@icomp.ufam.edu.br>
2025-01-26 19:16:44 -0400

    feat(rabbitmq): configurar filas de sucesso e erro com lógica específica para POST - Alteradas as classes de conexão com o RabbitMQ para suportar duas filas: sucesso e erro. - Criadas as filas  (sucesso) e  (erro). - Implementada lógica para enviar mensagens para a fila de sucesso apenas em requisições POST com status 2xx. - Mensagens de erro são enviadas para a fila de erro em todos os outros casos.

--- oracleebs/src/main/java/com/tech/teste/luiza/oracleebs/config/rabbit/MessageSenderImpl.java
+++ oracleebs/src/main/java/com/tech/teste/luiza/oracleebs/config/rabbit/MessageSenderImpl.java
@@ -1,9 +1,8 @@
 package com.tech.teste.luiza.oracleebs.config.rabbit;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingResponseWrapper;
@@ -15,17 +14,22 @@ import static com.tech.teste.luiza.oracleebs.util.HttpResponseProcessor.convertT
 @Component
 public class QueueSender implements MessageSender{
     private RabbitTemplate rabbitTemplate;
-    private Queue queue;
+    private final QueueDecider queueDecider;
 
-    public QueueSender(RabbitTemplate rabbitTemplate, Queue queue) {
+    public QueueSender(RabbitTemplate rabbitTemplate, QueueDecider queueDecider) {
         this.rabbitTemplate = rabbitTemplate;
-        this.queue = queue;
+        this.queueDecider = queueDecider;
     }
+
 
     @Override
-    public void send(ContentCachingResponseWrapper responseWrapper) throws IOException {
+    public void send(HttpServletRequest httpRequest, HttpServletResponse httpResponse, ContentCachingResponseWrapper responseWrapper) throws IOException {
         JsonNode responseBody = convertToJsonNodeResponseBody(responseWrapper);
-        rabbitTemplate.convertAndSend(this.queue.getName(), responseBody);
+        String queue= queueDecider.decideQueue(httpRequest,httpResponse);
+        rabbitTemplate.convertAndSend(queue, responseBody);
     }
+
+
+
 
 }
